fd28975effd0dc02680710c9823705e37b27329b
Luca De Feo <dfl@deagol>
2013-09-27 04:12:18 +0200

    Added very experimental TTS feature

--- irene-loic.js
+++ irene-loic.js
@@ -1,4 +1,18 @@
 $(function() {
+    var hashes = [
+	'#inquisition',
+	'#pendules',
+	'#simplicio',
+	'#observation',
+	'#decollage',
+	'#planetes',
+	'#satellites',
+	'#cometes',
+	'#rock',
+	'#supernovae',
+	'#retour'
+    ];
+
     var cues = {
 	'#inquisition': 1,
 	'#pendules': 30,
@@ -66,7 +80,7 @@ $(function() {
     for (hash in cues) {
         if (cues[hash] >= 0) {
             pop.cue(cues[hash]-1, function() {
-		pop.pause();
+		pop.pause().emit('cueend');
             });
         }
     }
@@ -82,6 +96,11 @@ $(function() {
 	    }
         }
 	return hash;
+    }
+
+    function scrollText(p) {
+	$('#text').scrollTop(p.offset().top 
+			     - $('#text>p').first().offset().top);
     }
     
     function updateText() {
@@ -99,9 +118,28 @@ $(function() {
 
 	// scroll text
 	if (hash) {
-	    $('#text').scrollTop(
-		tp.slice(bookmarks[hash], bookmarks[hash]+1)
-		    .offset().top - $('#text>p').first().offset().top);
+	    scrollText(tp.slice(bookmarks[hash], bookmarks[hash]+1));
+	}
+    }
+    
+    // Speak dialogue
+    function speak(start, end) {
+	if (start < end && $('#dospeak').prop('checked') && pop.paused()) {
+	    var sentence = $('#text>p').slice(start, start+1);
+	    setTimeout(function () { scrollText(sentence) }, 0);
+	    if ($('#char-' + sentence.data('voice')).prop('checked')) {
+		Popcorn(sentence.children('audio').get(0))
+		    .play()
+		    .on('ended', function() {
+			speak(start+1, end);
+		    });
+	    } else {
+		setTimeout(function() {
+		    speak(start+1, end);
+		}, sentence.text().length * 50);
+	    }
+	} else if (start == end && $('#dospeak').prop('checked')) {
+	    pop.emit('dialogend');
 	}
     }
 
@@ -110,6 +148,17 @@ $(function() {
 	.on('playing', updateText)
 	.on('pause', updateText)
 	.on('seeked', updateText)
+    // Say dialogue
+	.on('cueend', function() {
+	    var hash = currentHash();
+	    if (hash && hashes.indexOf(hash) > 0) {
+		var prev = hashes[hashes.indexOf(hash)-1];
+		speak(bookmarks[prev], bookmarks[hash]);
+	    }
+	})
+	.on('dialogend', function() {
+	    if (pop.paused()) pop.play();
+	})
     // On time change, look for the closest cue update playlist
 	.on('timeupdate', function() {
 	    var hash = currentHash();
@@ -120,14 +169,61 @@ $(function() {
 	});
 
     var conv = new Markdown.Converter();
+    var characters = {
+	"[comments]"     : "f1",
+	"L'inquisiteur"  : "m1",
+	"Galilée"        : "m2",
+	"Kepler"         : "m3",
+	"Simplicio"      : "m4",
+	"Cyrano"         : "m5",
+	"Jupiter"        : "f2",
+	"_"              : "m6"
+    };
 
     $.ajax({
 	url: 'Galilée-proces-dialogue.md',
 	dataType: 'text',
 	success: function (data) {
-	    $('#text').html(conv.makeHtml(data));
+	    $('#text')
+		.html(conv.makeHtml(data))
+		.find('p')
+		.each(function(i, p) {
+		    var $p = $(p);
+		    $p
+			.data('char', $p.find('strong').text() || "[comments]")
+			.data('voice', $p.data('char') in characters ? characters[$p.data('char')] : characters['_'])
+			.data('text', $p.text().replace(/;/g, ','));
+		});
 	}
     });
+
+    for (c in characters) {
+	$('#characters').append('<li><input type="checkbox" id="char-' + characters[c] + 
+				'" disabled checked="checked"/> ' + c + '</li>');
+    }
+
+    $('#dospeak')
+	.one('change', function() {
+	    $('#text>p').each(function(i, p) {
+		var $p = $(p);
+		$p.append('<audio src="http://wa.eguidedog.net/cgi-bin/ekho.pl?cmd=SAVEOGG&voice=fr+'
+			  + $p.data('voice') + '&speedDelta=0&pitchDelta=0&volumeDelta=0&text=' 
+			  + encodeURI($p.data('text')) + '">');
+	    });
+	})
+	.on('change', function() {
+	    if (this.checked) {
+		$('#characters')
+		    .removeClass('notspeaking')
+		    .find('input')
+		    .prop('disabled', false);
+	    } else {
+		$('#characters')
+		    .addClass('notspeaking')
+		    .find('input')
+		    .prop('disabled', true);
+	    }
+	});
 
     // Move video to a new window
     $('#video-container a').on('click', function() {
